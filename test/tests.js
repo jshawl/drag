a3f5164ea8678989fb4e6e5064361ddc07999c33
Jesse Shawl <jesse@jshawl.com>
2015-08-16 17:30:20 -0400

    comin back

--- test/tests.js
+++ test/tests.js
@@ -4,4 +4,13 @@ describe("drag", function(){
   it("is an object", function(){
     expect(drag).to.be.ok 
   })
+  it("has a mode", function(){
+    expect(drag.mode).to.be.ok
+  })
+  it("listens for events", function(){
+    expect(typeof drag.on).to.eq("function") 
+  })
+  it("emits events", function(){
+    expect(typeof drag.emit).to.eq("function") 
+  })
 })
